d43004ad20bbd5d09cff16cb9b2289559f93b8c3
Fritzie Primananda <primanandafritzie@gmail.com>
2021-08-22 15:52:35 +0800

    background

--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -2,9 +2,19 @@
     <head>
         <style>
             body {
-                background-image: url("images/gradient.svg");
-            }
+    position: absolute;
+    width: 1920px;
+    height: 1086px;
+    background-image: url("images/gradient.svg");
+    left: 0px;
+    top: -6px;
+    background-repeat: no-repeat;
+    background-attachment: fixed;
+    /* background: #FFFFFF; */
+    
+}
         </style>
+        <link rel="stylesheet" type="text/css" href="{{asset('css/style.css')}}">
     </head>
     <body>
         <h1>
--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -2,6 +2,22 @@
 
 @section('content') 
 
-    login
-    
+    <img src="" alt="">
+    <div class="gradient">
+        <div class="login_box">
+            <form action="#" method="post">
+               
+                    <label style="font-size: 12px; text-align: left; margin-left: 6%; font-weight: bolder;">Username</label><br>
+                    <input class="box_login" type="text" name="uname" placeholder="Username" required autocomplete="off">
+                <div class="box_login">
+                    <label style="font-size: 12px; text-align: left; margin-left: 6%; font-weight: bolder;">Password</label><br>
+                    <div class="login"><input type="Password" name="password" placeholder="Password" required></div>
+                </div>
+
+                <div class="button_login">
+                    <input type="submit" name="submit" value="Login" style="margin-bottom: 80px;">
+                </div>
+            </form>
+        </div>
+    </div>
 @endsection
