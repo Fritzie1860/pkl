3368cbc52bed722b154546788015dfc749ca8b86
a <a@a.a>
2021-08-24 14:24:57 +0800

    sidebar menu

--- resources/views/layout/sidebar.blade.php
+++ resources/views/layout/sidebar.blade.php
@@ -3,8 +3,27 @@
     <link rel="stylesheet" type="text/css" href="{{asset('css/style.css')}}">
     </head>
     <body>
-        <div class="side_bg" style="background-image: url('images/gradientberdiri.png')">
-        halo
+        <div class="side_bg"  >
+        <div class="sub_menu">
+        <img src="{{asset('images/home_fill.png')}}" > 
+            <p>
+                Dashboard
+            </p>
+        </div>
+
+        <div class="sub_menu">
+        <img src="{{asset('images/folder_fill.png')}}"> 
+            <p>
+                Project
+            </p>
+        </div>
+
+        <div class="sub_menu"> 
+            <img src="{{asset('images/users_fill.png')}}">
+            <p>
+                Users
+            </p>
+        </div>
         </div>
         
     </body>
