117dc73dc9c87eb37c80b9c7ab36545982cc6493
coild <xreza75@gmail.com>
2021-10-09 10:12:47 +0800

    project

---
+++ database/migrations/2021_10_09_014858_create_projects_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProjectsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('projects', function (Blueprint $table) {
+            $table->id();
+            $table->string('pro_nama', 125);
+            $table->date('tanggal_mulai');
+            $table->string('pro_status');
+            $table->timestamp('pro_update');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('projects');
+    }
+}
