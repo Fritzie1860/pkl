0bde86d447fdcfa1a2efa4dcdbfd5987a14a364c
Fritzie Primananda <primanandafritzie@gmail.com>
2021-10-13 02:46:41 +0800

    laper

---
+++ database/migrations/2021_10_12_182917_create_tos5data_table.php
@@ -0,0 +1,52 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTos5dataTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tos5data', function (Blueprint $table) {
+            $table->id('id_tos51', 5);
+            $table->string('type', 100);
+            $table->double('b', 5);
+            $table->double('h', 5);
+            $table->double('bd', 5);
+            $table->double('diatul_utama', 5);
+            $table->double('diatul_skg', 5);
+            $table->double('diatul_pg', 5);
+            $table->double('sengkang_dia_ka', 5);
+            $table->double('sengkang_dia_tg', 5);
+            $table->double('sengkang_dia_ki', 5);
+            $table->double('peminggang_dia', 5);
+            $table->double('peminggang_n', 5);
+            $table->double('jumtulut_atas1', 5);
+            $table->double('jumtulut_atas2', 5);
+            $table->double('jumtulut_atas3', 5);
+            $table->double('jumtulut_bawah1', 5);
+            $table->double('jumtulut_bawah2', 5);
+            $table->double('jumtulut_bawah3', 5);
+            $table->double('jarakseng_dia_ka', 5);
+            $table->double('jarakseng_dia_tg', 5);
+            $table->double('jarakseng_dia_ki', 5);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tos5data');
+    }
+}
---
+++ database/migrations/2021_10_12_183002_create_tos5summary_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTos5summaryTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tos5summary', function (Blueprint $table) {
+            $table->id('id_tos54', 5);
+            $table->string('nama', 100);
+            $table->double('beton',5);
+            $table->double('besi',5);
+            $table->double('bekisting',5);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tos5summary');
+    }
+}
