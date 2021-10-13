877165ac47a94e00f7aa64c17cc594b6c21190ab
Fritzie Primananda <primanandafritzie@gmail.com>
2021-10-13 14:35:30 +0800

    tos 6 belum rampung

---
+++ app/Models/tos6_a_r.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class tos6_a_r extends Model
+{
+    use HasFactory;
+}
---
+++ app/Models/tos7vrwbatu_kali.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class tos7vrwbatu_kali extends Model
+{
+    use HasFactory;
+}
