b42e3313adea6b2bec2a97df1814d7c1d2c4303d
coild <xreza75@gmail.com>
2021-12-09 23:56:51 +0800

    users

--- resources/views/users.blade.php
+++ resources/views/users.blade.php
@@ -3,9 +3,9 @@
     <title>Invest Islands || Users</title>
 @endsection
 <div id="wrapper">
-    <!-- @include('layout.sidebar') -->
+    
     <div id="page-wrapper" class="gray-bg">
-        <!-- @include('layout.header') -->
+        
         @include('layout.tabel_users')
     </div>
 </div>
